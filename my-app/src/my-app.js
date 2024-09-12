08a9485eb804b9506e2564cf0e8a8db5b09f0c9c
Nikhil79004 <mehranikhilsingh@gmail.com>
2024-09-12 20:56:18 +0530

    First Commit
    First Commit of Practice Project

---
+++ my-app/src/my-app.js
@@ -0,0 +1,72 @@
+import "./App.css";
+import Alert from "./components/Alert";
+import Navbar from "./components/Navbar";
+import About from "./components/About";
+import TextForm from "./components/TextForm";
+import React, {useState} from "react";
+import{
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+
+
+function App() {
+  const [mode, setMode] = useState('light');
+  const [alert, setAlert] = useState(null);
+  const showAlert=(message,type)=>{
+    setAlert({
+      msg : message,
+      type: type
+    })
+
+    setTimeout(() => {
+      setAlert(null);
+    }, 1000);
+  }
+  
+
+  // }
+  // const removeBodyClasses = ()=>{
+  //   document.body.classList.remove('bg-light')
+  //   document.body.classList.remove('bg-dark')
+  //   document.body.classList.remove('bg-warning')
+  //   document.body.classList.remove('bg-danger')
+  //   document.body.classList.remove('bg-success')
+  // }
+  // 
+
+    const toggleMode = ()=>{
+  //   removeBodyClasses();
+  //   document.body.classList.add('bg-'+ cls)
+    if (mode ==='light'){
+      setMode('dark');
+      document.body.style.backgroundColor = '#042743';
+      showAlert("Dark mode has been enabled","success");
+      // document.title = 'TextUtils-Dark Mode';
+    } 
+    else{
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+      showAlert("Light mode has been enabled","success");
+      // document.title = 'TextUtils-Light Mode';
+    }
+  }
+  return (
+    <>
+    {/* //   <Navbar title="Textutils" aboutText="About TextUtils"/> */}
+    {/* <Navbar/> */}
+    <Router>
+    <Navbar title="TextUtils" mode={mode} toggleMode ={toggleMode}/>
+    <Alert alert={alert}/>
+    <div className="container my-3">
+    <Routes>
+            <Route exact path="/about" element={<About mode={mode} />} />
+            <Route exact path="/" element={<TextForm showAlert={showAlert} heading="TextUtils - word counter, character counter, remove extra spaces" mode={mode} />} />
+    </Routes>
+    </div>
+    </Router>
+    </>
+  );
+}
+export default App;
